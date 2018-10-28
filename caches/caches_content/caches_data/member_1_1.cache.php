3c1082a5ebcfbf3b27b0b7637d42fe714e232284
Administrator <cxo114.com@163.com>
2018-10-28 10:11:54 +0800

    init

---
+++ caches/caches_content/caches_data/member_1_1.cache.php
@@ -0,0 +1,26 @@
+<?php
+return array (
+  0 => 
+  array (
+    'id' => '4',
+    'catid' => '6',
+    'title' => '中国国家画院“数字艺术研究所”“美育研究所”在京成立',
+    'url' => 'http://www.te.com/index.php?m=content&c=index&a=show&catid=6&id=4',
+    'username' => 'abc',
+    'sysadd' => '0',
+    'inputtime' => '1534343974',
+    'status' => '99',
+  ),
+  1 => 
+  array (
+    'id' => '3',
+    'catid' => '6',
+    'title' => 'abc t',
+    'url' => 'http://www.te.com/index.php?m=content&c=index&a=show&catid=6&id=3',
+    'username' => 'abc',
+    'sysadd' => '0',
+    'inputtime' => '1534340713',
+    'status' => '99',
+  ),
+);
+?>
